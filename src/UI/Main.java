e31965352c8c80e987bd183af8b7df5959431033
khalix4 <jeremiah.abu@binghamuni.edu.ng>
2023-05-21 21:21:16 +0100

    first commit

---
+++ src/UI/Main.java
@@ -0,0 +1,27 @@
+package UI;
+
+//import UI.BankGUI;
+//import UI.CurrentAccount;
+//import UI.SavingsAccount;
+//
+//import javax.swing.*;
+import javax.swing.*;
+import java.awt.*;
+import java.util.Scanner;
+
+
+
+
+
+public class
+Main {
+    public static void main(String[] args) {
+//       SwingUtilities.invokeLater(new Runnable() {
+//            @Override
+//            public void run() {
+        new JFrame();
+        WelcomePage welcomePage = new WelcomePage();
+        Frame.getFrames();
+
+    }
+}
---
+++ src/UI/WelcomePage.java
@@ -0,0 +1,45 @@
+package UI;
+import javax.swing.*;
+import java.awt.*;
+
+
+public class WelcomePage extends JFrame {
+    // Constructor
+    public WelcomePage() {
+        setTitle("Welcome Page");
+        setSize(400, 300);
+        setLocationRelativeTo(null);
+
+        // Set the default close operation
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        initComponents();
+        setVisible(true);
+    }
+
+    private void initComponents() {
+        // Create a panel to hold the components
+        JPanel panel = new JPanel();
+
+        // Set the layout manager for the panel
+        panel.setLayout(new BorderLayout());
+
+        // Create a label with welcome text
+        JLabel welcomeLabel = new JLabel("Welcome to Curry Bank");
+        JButton nextpageButton = new JButton("next page");
+        welcomeLabel.setFont(new Font("Arial", Font.BOLD, 24));
+        welcomeLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+        // Add the label to the panel
+        panel.add(welcomeLabel, BorderLayout.CENTER);
+
+        // Add the panel to the frame
+        add(panel);
+    }
+
+    // Main method to start the application
+    public static void main(String[] args) {
+        // Create an instance of the WelcomePage class
+        SwingUtilities.invokeLater(() -> new WelcomePage());
+    }
+}
+
