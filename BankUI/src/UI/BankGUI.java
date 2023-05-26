e064636a3a5148f0d7640a72558c74ab8bfcc2fe
Orji_TheDev <117165108+orjithedev@users.noreply.github.com>
2023-05-26 15:34:03 +0100

    Update BankGUI.java

--- BankUI/src/UI/BankGUI.java
+++ BankUI/src/UI/BankGUI.java
@@ -44,13 +44,56 @@ public class BankGUI extends JFrame {
         bankFrame.setLayout(new FlowLayout(FlowLayout.TRAILING));
         bankFrame.setVisible(true);
         
-        bankButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
+      Currentaccount1.add(CurrentAmount);
+        Currentaccount1.add(Currentaccount2);
 
-            }
+        Savingsaccount1.add(SavingsAmount);
+        Savingsaccount1.add(Savingsaccount2);
+
+        Currentaccount3.add(CurrentAmount1);
+        Currentaccount3.add(Currentaccount4);
+
+        Savingsaccount3.add(SavingsAmount1);
+        Savingsaccount3.add(Savingsaccount4);
+
+
+;
+        
+       bankButton.addActionListener(e -> {
+
+           CurrentAccount.setSize(200, 300);
+           CurrentAccount.setLayout(new FlowLayout(FlowLayout.CENTER));
+           CurrentAccount.setVisible(true);
+       });
+       bankButton2.addActionListener(e -> {
+           SavingsAccount.setSize(200,300);
+           SavingsAccount.setLayout(new FlowLayout(FlowLayout.CENTER));
+           SavingsAccount.setVisible(true);
+       });
+        Currentaccount.addActionListener(e -> {
+            Currentaccount1.setLayout(new FlowLayout(FlowLayout.CENTER));
+            Currentaccount1.setSize(200, 300);
+            Currentaccount1.setVisible(true);
+
+
         });
+        Savingsaccount.addActionListener(e -> {
+            Savingsaccount1.setSize(200, 300);
+            Savingsaccount1.setLayout(new FlowLayout(FlowLayout.CENTER));
+            Savingsaccount1.setVisible(true);
 
+
+        });
+        getCurrentaccount.addActionListener(e ->{
+            Currentaccount3.setSize(200, 300);
+            Currentaccount3.setLayout(new FlowLayout(FlowLayout.CENTER));
+            Currentaccount3.setVisible(true);
+        });
+        getSavingsaccount.addActionListener(e ->{
+            Savingsaccount3.setSize(200, 300);
+            Savingsaccount3.setLayout(new FlowLayout(FlowLayout.CENTER));
+            Savingsaccount3.setVisible(true);
+        });
 
 
 
